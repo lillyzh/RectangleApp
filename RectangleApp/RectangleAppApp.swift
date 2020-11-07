15544cdff1148101380449c2019de8ae2ba1c184
Lilly Zhou <lillyzh@stanford.edu>
2020-11-06 23:45:05 -0800

    Initial Commit

---
+++ RectangleApp/RectangleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RectangleAppApp.swift
+//  RectangleApp
+//
+//  Created by Lilly Zhou on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct RectangleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
